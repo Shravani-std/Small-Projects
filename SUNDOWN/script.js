13fc33901a1000c507de7a8bc3d29ccd2593b31c
Tobi-Bymistake <shravanitingare333@gmail.com>
2024-03-18 21:44:43 +0530

    commit

--- SUNDOWN/script.js
+++ SUNDOWN/script.js
@@ -2,3 +2,24 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+// var a = document.querySelector("#elem-1")
+// console.log(a)
+
+var elemC = document.querySelector("#row-container")
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter",function(){
+    // alert("hoo")
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave",function(){
+    // alert("hoo")
+    fixed.style.display = "none"
+})
+var elems = document.querySelectorAll(".elem")
+// console.log(elems)
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})` 
+    })
+})
